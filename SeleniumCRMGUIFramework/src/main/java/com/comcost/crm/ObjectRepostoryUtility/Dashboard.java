87036e06a0d3ab093113220e0927d4a41839f2c8
Nitin Pant <Nitin Pant@LAPTOP-QJS6JHDG>
2025-11-27 16:24:29 +0530

    push to gr

---
+++ SeleniumCRMGUIFramework/src/main/java/com/comcost/crm/ObjectRepostoryUtility/Dashboard.java
@@ -0,0 +1,27 @@
+package com.comcost.crm.ObjectRepostoryUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Dashboard {
+	WebDriver driver;
+	public Dashboard(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//a[text()='Dashboard']")
+	private WebElement DashboardbBTN;
+	public WebElement getDashboardbBTN() {
+		return DashboardbBTN;
+	}
+	
+	
+	
+	
+	
+	
+
+}
